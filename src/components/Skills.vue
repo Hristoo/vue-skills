82b5049b88d419b788f150858a64561fd4a5fb93
SEO & Web design Hristo Dimitrov <dimitrov_hristo@mail.bg>
2019-01-31 10:58:56 +0200

    Add files via upload

---
+++ src/components/Skills.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="hello">
+  <div class="holder">
+    <ul>
+      <li v-for="(data, index) in skills" :key='index'>{{index}}. {{data.skill}}</li>
+    </ul>
+    <p v-if="skills.length >= 1"> More than one</p>
+    <p v-else>Less than one</p>
+  </div> 
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Skills',
+  data(){
+    return{
+      skills: [
+        {"skill": "Vue.js"},
+        {"skill": "Frontend"},
+
+      ]
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
