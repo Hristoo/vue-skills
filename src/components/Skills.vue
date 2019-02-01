0740de827ccdb067f0d5d01e608979cb567ccb6f
SEO & Web design Hristo Dimitrov <dimitrov_hristo@mail.bg>
2019-02-01 15:05:16 +0200

    validation

--- src/components/Skills.vue
+++ src/components/Skills.vue
@@ -1,6 +1,13 @@
 <template>
   <div class="container">
     <div class="holder">
+
+      <form @submit.prevent="addSkill">
+
+       <input type="text" placeholder="Enter a skill you have.." v-model="skill" v-validate="'min:5'" name="skill">
+      <p class="alert" v-if="errors.has('skill')">{{ errors.first('skill') }}</p>
+      </form>
+
       <ul>
         <li v-for="(data, index) in skills" :key='index'>{{data.skill}}</li>
       </ul>
@@ -14,15 +21,25 @@ export default {
   name: 'Skills',
   data(){
     return{
+      skill: '',
       skills: [
         {"skill": "Vue.js"},
         {"skill": "Frontend"}
-      ],
-      bgColor: 'yellow',
-      bgWight: '100%',
-      bgHeight: '30px'
-      
+      ]      
     }
+  },
+    methods: {
+      addSkill() {
+      this.$validator.validateAll().then((result) =>{
+        if(result){
+          this.skills.push({skill: this.skill})
+          this.skill = '';
+        }else {
+          console.log('Not valid');
+        }
+      })
+   
+      }
   }
 }
 </script>
@@ -56,5 +73,22 @@ export default {
 
   .container {
     box-shadow: 0px 0px 40px lightgray;
+  }
+
+    input {
+    width: calc(100% - 40px);
+    border: 0;
+    padding: 20px;
+    font-size: 1.3em;
+    background-color: #323333;
+    color: #687F7F;
+  }
+
+  .alert {
+    background: #fdf2ce;
+    font-weight: bold;
+    display: inline-block;
+    padding: 5px;
+    margin-top: -20px;
   }
 </style>
